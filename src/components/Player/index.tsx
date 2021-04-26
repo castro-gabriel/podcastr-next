5bf6158ee7ebbf9dfde8764979c9c921be09905a
Gabriel Castro <gabrielcastromail@gmail.com>
2021-04-25 22:24:09 -0300

    Styled player slider

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,10 @@
+import Image from 'next/image';
 import { useContext } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 import { PlayerContext } from '../../contexts/PlayerContext';
+
 import styles from './styles.module.scss';
 
 export function Player() {
@@ -12,18 +17,34 @@ export function Player() {
         <div className={styles.playerContainer}>
             <header>
                 <img src="/playing.svg" alt="Playing"/>
-                <strong>Tocando agora {episode?.title}</strong>
+                <strong>Tocando agora</strong>
             </header>
 
-            <div className={styles.emptyPlayer}>
-                <strong>Selecione um podcast para ouvir</strong>
-            </div>
+            { episode ? (
+                <div className={styles.currentEpisode}>
+                    <Image width={592} height={592} src={episode.thumbnail} objectFit="cover"/>
+                    <strong>{episode.title}</strong>
+                    <span>{episode.members}</span>
+                </div>
+            ) : (
+                <div className={styles.emptyPlayer}>
+                    <strong>Selecione um podcast para ouvir</strong>
+                </div>
+            ) }
 
-            <footer className={styles.empty}>
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider   }>
-                        <div className={styles.emptySlider} />
+                        { episode ? (
+                            <Slider 
+                                trackStyle={{ backgroundColor: '#04d361' }}
+                                railStyle={{ backgroundColor: '#9f75ff' }}
+                                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+                            />
+                        ) : (
+                            <div className={styles.emptySlider} />
+                        ) }
                     </div>
                     <span>00:00</span>
                 </div>
